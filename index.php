f33abd6c70b0c2a4f6583ea94e5fbbebc1df4950
pablocrdozo <53062091+pablocrdozo@users.noreply.github.com>
2021-11-30 21:43:56 -0500

    se creo la clase errores - respuestas

--- index.php
+++ index.php
@@ -4,8 +4,18 @@ require_once "clases/conexion/conexion.php";
 
 $conexion = new conexion;
 
-$query = "Select * from pacientes";
+// $query = "Select * from pacientes";
 
-print_r($conexion->obtenerDatos($query));
+// print_r($conexion->obtenerDatos($query));
+
+// $query = "INSERT INTO pacientes (DNI) value ('0')";
+
+// print_r($conexion->nonQuery($query));
+
+// $query = "INSERT INTO pacientes (DNI) value ('1')";
+
+// print_r($conexion->nonQueryId($query))
 
 ?>
+
+Hola index
